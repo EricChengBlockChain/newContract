e482a917cc03802989a7b300a8c7d1691d6ca3d5
Eric.Cheng.BlockChain <matsumi_design@163.com>
2020-12-28 21:52:41 +0800

    WETH	fWETH
    WETH	fWETH

--- VaultProxy.sol
+++ VaultProxy.sol
@@ -25,7 +25,7 @@ pragma solidity ^0.5.0;
  * @title Proxy
  * @dev Implements delegation of calls to other contracts, with proper
  * forwarding of return values and bubbling of failures.
- * It defines a fallback function that delegates all calls to the address
+ * It defines a fallback function that delegates all calls to the address.
  * returned by the abstract _implementation() internal function.
  */
 contract Proxy {
